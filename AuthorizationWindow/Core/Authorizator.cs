17e787693868693af842af61853f26be65564981
Daniil Solovev <solovevdaniil01@gmail.com>
2022-09-23 00:20:32 +0300

    Добавьте файлы проекта.

---
+++ AuthorizationWindow/Core/Authorizator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+
+namespace AuthorizationWindow.Core
+{
+    public class Authorizator
+    {
+        public bool Authorization()
+        {
+            
+        }
+    }
+}
